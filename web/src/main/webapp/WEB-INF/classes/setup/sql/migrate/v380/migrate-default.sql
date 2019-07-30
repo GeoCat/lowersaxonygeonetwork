9ddbeb5045fcaa72891e935845fe1446b7f99eb9
Antonio C <antonio.cerciello@geocat.net>
2019-07-30 15:58:20 +0200

    Version 3.8.0-SNAPSHOT

---
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v380/migrate-default.sql
@@ -0,0 +1,4 @@
+UPDATE Settings SET value='3.8.0' WHERE name='system/platform/version';
+UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+
+-- System tuning for NiederSachsen
